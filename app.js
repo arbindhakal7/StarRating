b92713294bcede8b3e825cc2e30472e6c8b775ee
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-19 11:51:22 +0545

    :construction: logic being constructed

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const allStars = document.querySelectorAll('.star');
+allStars.forEach((star, i) =>{
+    star.onclick = function(){
+        let current_star_rating = i + 1
+
+        allStars.forEach((star, j)=>{
+            if(current_star_rating >= j+1){
+                
+            }
+        })
+    }
+})
