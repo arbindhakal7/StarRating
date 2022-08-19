b849c276753674971d4ec0e050fd2db409da4600
arbindhakal7 <arbin.dhakal123@gmail.com>
2022-08-19 12:39:48 +0545

    :sparkles: project completed

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const allStars = document.querySelectorAll('.star');
-let current_star_level = document.querySelector('.current_rating')
+const allStars = document.querySelectorAll(".star");
+let current_star_level = document.querySelector(".current_rating");
 
-allStars.forEach((star, i) =>{
-    star.onclick = function(){
-        let current_star_rating = i + 1
-        current_star_level.innerText = `${current_star_rating} out of 5`
+allStars.forEach((star, i) => {
+  star.onclick = function () {
+    let current_star_rating = i + 1;
+    current_star_level.innerText = `${current_star_rating} out of 5`;
 
-        allStars.forEach((star, j)=>{
-            if(current_star_rating >= j+1){
-                star.innerHTML = '&#9733'
-            }
-            else{
-                star.innerHTML = '&#9734'
-            }
-        })
-    }
-})
+    allStars.forEach((star, j) => {
+      if (current_star_rating >= j + 1) {
+        star.innerHTML = "&#9733";
+      } else {
+        star.innerHTML = "&#9734";
+      }
+    });
+  };
+});
